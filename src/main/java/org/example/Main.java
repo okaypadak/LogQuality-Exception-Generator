d12a3e257034f838d14aac90c275adfb499e7501
kypdk@proton.me <kypdk@proton.me>
2023-12-12 15:16:44 +0300

    iç içe sınıf ve metodlar

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,17 +7,15 @@ import java.sql.SQLException;
 import java.util.Timer;
 import java.util.TimerTask;
 
+
 public class Main {
 
-    private static final String LOG_FILE_NAME = "log.txt";
 
     public static void main(String[] args) {
         Timer timer = new Timer();
-        timer.schedule(new ThrowExceptionsTask(), 0, 60000); // Dakikada bir çalıştır
-
-        // Uygulamayı çalışır durumda tutmak için ekledik, genellikle gerekmez
+        timer.schedule(new ThrowExceptionsTask(), 0, 60000);
         try {
-            Thread.sleep(600000); // 10 dakika boyunca çalıştır
+            Thread.sleep(600000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
@@ -26,50 +24,67 @@ public class Main {
     static class ThrowExceptionsTask extends TimerTask {
         @Override
         public void run() {
-            try {
-                // RuntimeException fırlat
-                throw new RuntimeException("Dakikada bir runtime exception fırlatıldı!");
-            } catch (RuntimeException e) {
-                // RuntimeException'ı log dosyasına ve konsola yaz
-                logException(e, "ERROR");
-            }
 
-            try {
-                // IOException fırlat
-                throw new IOException("Dakikada bir IO exception fırlatıldı!");
-            } catch (IOException e) {
-                // IOException'ı log dosyasına ve konsola yaz
-                logException(e, "ERROR");
-            }
+            RunMethods runMethods = new RunMethods();
 
-            try {
-                // SQLException fırlat
-                throw new SQLException("Dakikada bir SQL exception fırlatıldı!");
-            } catch (SQLException e) {
-                // SQLException'ı log dosyasına ve konsola yaz
-                logException(e, "ERROR");
-            }
-
-            try {
-                // UnsupportedOperationException ile bir uyarı (warn) fırlat
-                throw new UnsupportedOperationException("Dakikada bir uyarı (warn) fırlatıldı!");
-            } catch (UnsupportedOperationException e) {
-                // Uyarıyı log dosyasına ve konsola yaz
-                logException(e, "WARN");
-            }
+            runMethods.databaseMethod();
+            runMethods.runtimeMethod();
+            runMethods.ioMethod();
+            runMethods.warnMethod();
         }
 
-        private void logException(Exception e, String logLevel) {
-            try (PrintWriter writer = new PrintWriter(new FileWriter(LOG_FILE_NAME, true))) {
-                // Log dosyasına yaz
-                writer.println(logLevel + " - Exception: " + e.getMessage());
-                e.printStackTrace(writer);
 
-                // Konsola yaz
-                System.out.println(logLevel + " - Exception logged: " + e.getMessage());
-            } catch (IOException ex) {
-                ex.printStackTrace();
-            }
+    }
+
+
+}
+
+class RunMethods {
+    public void databaseMethod() {
+        try {
+            throw new SQLException("Dakikada bir SQL exception fırlatıldı!");
+        } catch (SQLException e) {
+            logException(e, "ERROR");
         }
     }
+
+    public void runtimeMethod() {
+        try {
+            throw new RuntimeException("Dakikada bir runtime exception fırlatıldı!");
+        } catch (RuntimeException e) {
+            logException(e, "ERROR");
+        }
+    }
+
+    public void ioMethod() {
+        try {
+            // IOException fırlat
+            throw new IOException("Dakikada bir IO exception fırlatıldı!");
+        } catch (IOException e) {
+            logException(e, "ERROR");
+        }
+    }
+
+    public void warnMethod(){
+        try {
+            throw new UnsupportedOperationException("Dakikada bir uyarı (warn) fırlatıldı!");
+        } catch (UnsupportedOperationException e) {
+            logException(e, "WARN");
+        }
+    }
+
+    private static final String LOG_FILE_NAME = "log.txt";
+    private void logException(Exception e, String logLevel) {
+        try (PrintWriter writer = new PrintWriter(new FileWriter(LOG_FILE_NAME, true))) {
+            // Log dosyasına yaz
+            writer.println(logLevel + " - Exception: " + e.getMessage());
+            e.printStackTrace(writer);
+
+            // Konsola yaz
+            System.out.println(logLevel + " - Exception logged: " + e.getMessage());
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
 }
